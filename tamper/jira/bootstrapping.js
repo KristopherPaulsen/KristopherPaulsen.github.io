013f85765996c8b54ff0762ed16a1dd8dc67a6ef
Kristopher Paulsen <kpaulsen@bluehost.com>
2017-06-05 15:53:45 +0000

    master:

---
+++ tamper/jira/bootstrapping.js
@@ -0,0 +1,4 @@
+fetch("https://gist.githubusercontent.com/KristopherPaulsen/006db773efaf0f7bef293845d94eaf8c/raw/8665c2e96cfe8506d9716e53084d388174884dd3/new_gist_file_0")
+.then((response) => response.text())
+.then((text) => eval(text))
+.then(console.log(test);
