6a7b2c5930b226b5b55dc346646737885528d031
Kristopher Paulsen <kpaulsen@bluehost.com>
2017-07-28 12:42:27 +0000

    master: don't need

--- tamper/jira/bootstrapping.js
+++
@@ -1,6 +0,0 @@
-var test;
-
-fetch("https://gist.githubusercontent.com/KristopherPaulsen/006db773efaf0f7bef293845d94eaf8c/raw/6b4f9f23724703c7d82b4872605df1be0cd34a01/new_gist_file_0")
-.then((response) => response.text())
-.then((text) => eval(text))
-.then(alert(test));
--- tamper/jira/hide_declined.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(() => {
-    $(".pullrequest-row").initialize(function() {
-        $(this).each(() => {
-            if ($(this).html().includes("DECLINED")){
-                $(this).hide();
-            }
-        });
-    });
-});
